82f2a9bbcd6588d7be5e5a4f93eb9d7661fb711e
Donny-Hek <69718795+Donny-Hek@users.noreply.github.com>
2025-04-16 23:07:34 +0300

    Create IraBackendApplication.java

---
+++ backend/src/main/java/com/IraBackendApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+@EnableScheduling
+public class IraBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IraBackendApplication.class, args);
+    }
+}
