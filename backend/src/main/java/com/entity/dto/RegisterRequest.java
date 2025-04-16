a0c8aa47aa0d1e31816910486f32c656d04a609d
Donny-Hek <69718795+Donny-Hek@users.noreply.github.com>
2025-04-16 23:10:48 +0300

    Create RegisterRequest.java

---
+++ backend/src/main/java/com/entity/dto/RegisterRequest.java
@@ -0,0 +1,8 @@
+@Data
+public class RegisterRequest {
+    private String login;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private Role role;
+}
